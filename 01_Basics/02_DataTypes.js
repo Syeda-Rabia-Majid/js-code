66150029268bab58323400112a127bcfcbfa5db6
Syeda-Rabia-Majid <syedarabiamajid@gmail.com>
2024-07-30 23:10:54 +0500

    Create 02_DataTypes.js

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; 
+//treat all js code as newer  version
+//alert(3+3);we are using vs code not browser//
+
+
+/*
+DataTypes
+let number => 2 to power 53
+BigInt => large numeric value
+let string =>""
+let boolean => true/false
+null => standalone value type(object)
+undefined => placeholder for a value which is not defined yets
+symbol => to check uniqueness of component
+*/
+
+let name = "RABIA"
+let age = 22
+let isLoggedIn =false
+let state;
+
+console.log(typeof null);  //object
+console.log(typeof undefined);   //undefined
+console.log(typeof BigInt);    //function
+
